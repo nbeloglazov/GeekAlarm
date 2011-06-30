619570e2171c4511da8029c528e02272be9b3c87
Nikita Beloglazov <nikelandjelo@gmail.com>
2011-06-30 09:01:20 +0300

    Adding server project.

---
+++ server/project.clj
@@ -0,0 +1,11 @@
+(defproject geekalarm-server "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+		 [org.clojure/clojure-contrib "1.2.0"]
+		 [ring "0.3.8"]
+		 [net.cgrand/moustache "1.0.0"]]
+  :dev-dependencies [[swank-clojure "1.2.0"]
+		     [lein-ring "0.4.0"]
+		     [ring-serve "0.1.0"]]
+  :ring {:handler geekalarm-server.server/handler})
+
