437f08b835f27dac5929c9b90a41be20a6411f4e
Nikita Beloglazov <nikelandjelo@gmail.com>
2011-07-25 21:14:47 -0700

    Adding ability to set alarm and play music.

--- android/src/nbeloglazov/geekalarm/android/activities/AlarmsActivity.java
+++ android/src/nbeloglazov/geekalarm/android/activities/AlarmsActivity.java
@@ -1,14 +1,22 @@
 package nbeloglazov.geekalarm.android.activities;
 
+import java.text.DateFormat;
+import java.util.Calendar;
+
 import nbeloglazov.geekalarm.android.R;
 import nbeloglazov.geekalarm.android.R.layout;
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.AlertDialog;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 
 public class AlarmsActivity extends Activity {
 
@@ -26,17 +34,39 @@ public class AlarmsActivity extends Activity {
         setContentView(R.layout.alarms);
         Button testButton = (Button)this.findViewById(R.id.test_button);
         testButton.setOnClickListener(new TestButtonListener());
+        findViewById(R.id.set_time).setOnClickListener(new SetAlarmListener());
 		Log.i(TAG, "onCreate");
     }
     
     private class TestButtonListener implements OnClickListener {
-
 		@Override
 		public void onClick(View v) {
 			Intent intent = new Intent(AlarmsActivity.this, TaskActivity.class);
 			startActivity(intent);
 		}
-    	
+    }
+    
+    private class SetAlarmListener implements OnClickListener {
+		@Override
+		public void onClick(View v) {
+			TimePicker picker = (TimePicker)findViewById(R.id.date_picker);
+			Calendar cur = Calendar.getInstance();
+			Calendar next = Calendar.getInstance();
+			next.set(Calendar.MINUTE, picker.getCurrentMinute());
+			next.set(Calendar.HOUR_OF_DAY, picker.getCurrentHour());
+			if (next.before(cur)) {
+				next.set(Calendar.DAY_OF_MONTH,
+						next.get(Calendar.DAY_OF_MONTH) + 1);
+			}
+			DateFormat formatter = DateFormat.getDateTimeInstance();
+			AlertDialog dialog = new AlertDialog.Builder(AlarmsActivity.this).setMessage(formatter.format(next.getTime())).create();
+			dialog.show();
+			Intent intent = new Intent(getApplicationContext(), TaskActivity.class);
+			PendingIntent pending = PendingIntent.getActivity(getApplicationContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+			AlarmManager manager = (AlarmManager)getSystemService(ALARM_SERVICE);
+			manager.set(AlarmManager.RTC_WAKEUP, next.getTimeInMillis(), pending);
+			finish();
+		}	
     }
 }
 
--- android/src/nbeloglazov/geekalarm/android/activities/TaskActivity.java
+++ android/src/nbeloglazov/geekalarm/android/activities/TaskActivity.java
@@ -1,12 +1,13 @@
 package nbeloglazov.geekalarm.android.activities;
 
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import nbeloglazov.geekalarm.android.R;
 import nbeloglazov.geekalarm.android.tasks.Category;
@@ -14,25 +15,30 @@ import nbeloglazov.geekalarm.android.tasks.Configuration;
 import nbeloglazov.geekalarm.android.tasks.Task;
 import nbeloglazov.geekalarm.android.tasks.TaskManager;
 import android.app.Activity;
-import android.graphics.Bitmap;
+import android.media.MediaPlayer;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.ImageView.ScaleType;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class TaskActivity extends Activity {
-	
+
 	private static final int MAX_NUM_OF_TASKS = 5;
-	
+	private static final int TASKS_TO_FINISH = 3;
+	private static final long PLAY_DELAY = 30 * 1000; // 30 seconds 
+
 	private boolean isWaitingForTask;
 	private Queue<Task> availableTasks;
 	private ChoiceListener choiceListener;
 	private int correctChoiceId;
 	private TaskLoader loader;
-	
+	private MediaPlayer player;
+	private int solved;
+	private int all;
+	private Timer timer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -42,34 +48,50 @@ public class TaskActivity extends Activity {
 		isWaitingForTask = true;
 		loader = new TaskLoader();
 		loader.execute(Configuration.getDefaultConfiguration());
+		player = MediaPlayer.create(this, R.raw.mario);
 		choiceListener = new ChoiceListener();
+		timer = new Timer();
+		findViewById(R.id.mute_button).setOnClickListener(new MuteListener());
+		updateStats();
 	}
-	
+
 	private void displayTask(Task task) {
-		ImageView question = (ImageView)findViewById(R.id.task_question);
+		ImageView question = (ImageView) findViewById(R.id.task_question);
 		question.setImageBitmap(task.getQuestion());
-		int choicesIds[] = {R.id.task_choice_1, 
-				            R.id.task_choice_2,
-				            R.id.task_choice_3,
-				            R.id.task_choice_4};
+		int choicesIds[] = { R.id.task_choice_1, R.id.task_choice_2,
+				R.id.task_choice_3, R.id.task_choice_4 };
 		correctChoiceId = choicesIds[task.getCorrect() - 1];
 		for (int i = 0; i < 4; i++) {
-			ImageView choiceView = (ImageView)findViewById(choicesIds[i]);
+			ImageView choiceView = (ImageView) findViewById(choicesIds[i]);
 			choiceView.setOnClickListener(choiceListener);
 			choiceView.setImageBitmap(task.getChoice(i));
 		}
+	}
+
+	private void updateStats() {
+		TextView view = (TextView) findViewById(R.id.stat_text);
+		view.setText(String.format("%d/%d", solved, all));
 	}
 	
 	private class ChoiceListener implements View.OnClickListener {
 
 		@Override
 		public void onClick(View v) {
-			Toast.makeText(getApplicationContext(), v.getId() == correctChoiceId ? "Accepted" : "Wrong answer", Toast.LENGTH_SHORT).show();
+			all++;
+			solved += v.getId() == correctChoiceId ? 1 : 0;
+			if (solved == TASKS_TO_FINISH) {
+				TaskActivity.this.finish();
+				return;
+			}
+			updateStats();
+			Toast.makeText(getApplicationContext(),
+					v.getId() == correctChoiceId ? "Accepted" : "Wrong answer",
+					Toast.LENGTH_SHORT).show();
 			if (availableTasks.isEmpty()) {
 				if (loader.getStatus() == AsyncTask.Status.FINISHED) {
 					loader = new TaskLoader();
 					loader.execute(Configuration.getDefaultConfiguration());
-				} 
+				}
 				isWaitingForTask = true;
 			} else {
 				displayTask(availableTasks.poll());
@@ -77,23 +99,32 @@ public class TaskActivity extends Activity {
 		}
 	}
 	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		timer.cancel();
+	}
+
 	private class TaskLoader extends AsyncTask<Configuration, Task, Void> {
 
 		@Override
 		protected Void doInBackground(Configuration... params) {
 			Configuration conf = params[0];
-			List<Map.Entry<Category, Integer>> categories = new ArrayList(conf.getCategories().entrySet());
+			List<Map.Entry<Category, Integer>> categories = new ArrayList(conf
+					.getCategories().entrySet());
 			Collections.shuffle(categories);
 			for (int i = 0; i < MAX_NUM_OF_TASKS; i++) {
-				Map.Entry<Category, Integer> taskType = categories.get(i % categories.size());
+				Map.Entry<Category, Integer> taskType = categories.get(i
+						% categories.size());
 				try {
-					Task task = TaskManager.getTask(taskType.getKey(), taskType.getValue());
+					Task task = TaskManager.getTask(taskType.getKey(),
+							taskType.getValue());
 					publishProgress(task);
 				} catch (Exception e) {
 					Log.e(TaskLoader.class.getName(), "Something bad", e);
 				}
 			}
-			return null;	
+			return null;
 		}
 
 		@Override
@@ -102,12 +133,31 @@ public class TaskActivity extends Activity {
 				availableTasks.add(task);
 			}
 			if (isWaitingForTask) {
+				player.start();
 				Task task = availableTasks.poll();
 				displayTask(task);
 				isWaitingForTask = false;
 			}
-		}	
-		
+		}
+	}
+
+	private class MuteListener implements View.OnClickListener {
+
+		@Override
+		public void onClick(View v) {
+			if (player.isPlaying()) {
+				player.pause();
+				timer.schedule(new ContinuePlayTask(), PLAY_DELAY);
+			}
+		}
+	}
+
+	private class ContinuePlayTask extends TimerTask {
+
+		@Override
+		public void run() {
+			player.start();
+		}
 	}
 
 }
