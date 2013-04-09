af0e06959812eba057ac5d7edd9ac5ef14746e9a
Nikita Beloglazov <nikelandjelo@gmail.com>
2013-04-09 13:50:04 +0300

    fix content types. They were missing.

--- server/src/com/geekalarm/server/server.clj
+++ server/src/com/geekalarm/server/server.clj
@@ -1,15 +1,15 @@
 (ns com.geekalarm.server.server
   (:import [java.util Date])
   (:use [net.cgrand.moustache :only [app delegate]]
-	[ring.middleware
-	 [params :only (wrap-params)]
-	 [keyword-params :only (wrap-keyword-params)]])
+        [ring.middleware
+         [params :only (wrap-params)]
+         [keyword-params :only (wrap-keyword-params)]])
   (:require [com.geekalarm.server
              [render-utils :as render]
-	     [mathml-utils :as mathml]
-	     [task-manager :as manager]
-	     [utils :as utils]]
-	    [clojure.data.json :as json]))
+             [mathml-utils :as mathml]
+             [task-manager :as manager]
+             [utils :as utils]]
+            [clojure.data.json :as json]))
 
 
 (def task-timeout (* 10 60 1000))
@@ -20,11 +20,11 @@
 
 (defn remove-expired-tasks [tasks]
   (let [lower-bound (- (.getTime (Date.))
-		       task-timeout)]
+                       task-timeout)]
     (->> tasks
-	 (filter (fn [[id task]]
-		   (>= (:timestamp task) lower-bound)))
-	 (into {}))))
+         (filter (fn [[id task]]
+                   (>= (:timestamp task) lower-bound)))
+         (into {}))))
 
 (defn run-collector []
   (utils/start-timer #(swap! active-tasks remove-expired-tasks)
@@ -36,16 +36,16 @@
 (defn response [body content]
   {:status 200
    :body body
-   :content content})
+   :headers {"Content-Type" content}})
 
 (defn get-image [request]
   (let [{:keys [id type number]} (:params request)
-	{:keys [question choices]} (@active-tasks id)]
+        {:keys [question choices]} (@active-tasks id)]
     (-> (if (= type "question")
-	  question
-	  (nth choices (Integer/parseInt number)))
+          question
+          (nth choices (Integer/parseInt number)))
         (java.io.ByteArrayInputStream.)
-	(response "image/png"))))
+        (response "image/png"))))
 
 (defn tasks-info [_]
   (-> (manager/tasks-info)
@@ -54,13 +54,13 @@
 
 (defn generate-task [request]
   (let [{:keys [type level]} (:params request)
-	task (manager/generate-task (keyword type)
+        task (manager/generate-task (keyword type)
                                     (dec (Integer/parseInt level)))
-	id (get-id)]
+        id (get-id)]
     (->> (assoc task
            :timestamp (.getTime (Date.))
            :level level)
-	 (swap! active-tasks assoc id))
+         (swap! active-tasks assoc id))
     (-> {:correct (:correct task)
          :id id}
         (json/json-str)
@@ -68,18 +68,18 @@
 
 (defn get-static-file [name]
   (let [th (Thread/currentThread)
-	loader (.getContextClassLoader th)]
+        loader (.getContextClassLoader th)]
     (.getResourceAsStream loader (str "static/" name))))
 
 (defn get-index-html [request]
   (response (get-static-file "index.html")
-	    "text/html"))
+            "text/html"))
 
 (defn add-result [request])
 
 (def handler
-     (app wrap-params wrap-keyword-params
-      [""] get-index-html
-      ["task"] generate-task
-      ["image"] get-image
-      ["tasks"] tasks-info))
+  (app wrap-params wrap-keyword-params
+       [""] get-index-html
+       ["task"] generate-task
+       ["image"] get-image
+       ["tasks"] tasks-info))
