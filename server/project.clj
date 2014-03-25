57494a88ae11751fa20ca284385beac6cdd62b14
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-03-25 23:15:49 +0000

    Upgrade dependencies.

--- server/project.clj
+++ server/project.clj
@@ -1,18 +1,18 @@
 (defproject geekalarm-server "1.0.1"
   :description "Server for generating and serving tasks."
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/data.json "0.2.1"]
-                 [org.clojure/math.numeric-tower "0.0.2"]
-                 [org.clojure/math.combinatorics "0.0.3"]
-                 [org.clojure/core.incubator "0.1.2"]
-                 [ring "1.1.8"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.json "0.2.4"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [org.clojure/math.combinatorics "0.0.7"]
+                 [org.clojure/core.incubator "0.1.3"]
+                 [ring "1.2.2"]
                  [net.cgrand/moustache "1.1.0"]
-                 [org.clojure/core.match "0.2.0-alpha8"]
-                 [incanter/incanter-core "1.4.0"]
-                 [org.scilab.forge/jlatexmath "0.9.6"]
+                 [org.clojure/core.match "0.2.1"]
+                 [incanter/incanter-core "1.5.4"]
+                 [org.scilab.forge/jlatexmath "1.0.2"]
                  [dk.brics.automaton/automaton "1.11.2"]]
-  :plugins [[lein-ring "0.8.2"]
-            [lein-immutant "0.17.1"]]
+  :plugins [[lein-ring "0.8.10"]
+            [lein-immutant "1.2.0"]]
   :ring {:handler com.geekalarm.server.server/handler
          :init com.geekalarm.server.server/run-collector}
   :uberjar-name "root.war")
